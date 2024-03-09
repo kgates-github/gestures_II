fd7b6afcaea80bf136f3baf00f1d0a81ae5a0216
Kevin Gates <kgates@gmail.com>
2024-03-08 16:35:11 -0800

    Get mediapipe functioning

--- app/src/components/TestComponent.js
+++ app/src/components/TestComponent.js
@@ -2,13 +2,19 @@ import React, { useEffect, useRef, useState } from 'react';
 import { GestureRecognizer, FilesetResolver } from '@mediapipe/tasks-vision';
 
 function TestComponent() {
-  const [webCamRunning, setWebcamRunning] = useState(false);
+  const [introOneDisplay, setIntroOneDisplay] = useState('flex');
+  const [introTwoDisplay, setIntroTwoDisplay] = useState('none');
+  const [introThreeDisplay, setIntroThreeDisplay] = useState('none');
+  const [introFourDisplay, setIntroFourDisplay] = useState('none');
+  const [webcamRunning, setWebcamRunning] = useState(false);
   let gestureRecognizer;
-  let webcamRunning = false;
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const enableWebcamButtonRef = useRef(null);
   const runningMode = "worker";
+  let lastVideoTime = null;
+  let categoryName = '';
+  let results;
   
   useEffect(() => {
     const video = videoRef.current;
@@ -22,9 +28,10 @@ function TestComponent() {
           modelAssetPath: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
           delegate: "GPU"
         },
-        runningMode: runningMode
+        runningMode: runningMode,
       });
-      
+      setIntroOneDisplay('none');
+      setIntroTwoDisplay('flex');
       
       // Check if webcam access is supported.
       function hasGetUserMedia() {
@@ -32,7 +39,6 @@ function TestComponent() {
       }
       
       if (hasGetUserMedia()) {
-       //enableWebcamButton = document.getElementById("webcamButton");
         enableWebcamButton.addEventListener("click", enableCam);
       } else {
         console.warn("getUserMedia() is not supported by your browser");
@@ -40,14 +46,8 @@ function TestComponent() {
 
       // Enable the live webcam view and start detection.
       function enableCam(event) {
-        if (!gestureRecognizer) {
-          alert("Please wait for gestureRecognizer to load");
-          return;
-        }
-        
-        setWebcamRunning(true);
-          //webcamButtonContainer.style.display = "none";
-          //document.getElementById('loading-01').style.display = "flex";
+        setIntroTwoDisplay('none');
+        setIntroThreeDisplay('flex');
         
         // getUsermedia parameters.
         const constraints = {
@@ -61,35 +61,40 @@ function TestComponent() {
       }
       async function predictWebcam() {
         const webcamElement = document.getElementById("webcam");
+        setWebcamRunning(true);
+        setIntroThreeDisplay('none');
+        setIntroFourDisplay('flex');
         
-        /*
-        // Now let's start detecting the stream.
+        // Start detecting the stream.
         if (runningMode === "IMAGE") {
           runningMode = "VIDEO";
-          await gestureRecognizer.setOptions({ runningMode: "VIDEO", numHands:2 });
+          await gestureRecognizer.setOptions({ runningMode: "VIDEO", numHands: 1 });
         }
+
         let nowInMs = Date.now();
         if (video.currentTime !== lastVideoTime) {
           lastVideoTime = video.currentTime;
           results = gestureRecognizer.recognizeForVideo(video, nowInMs);
         }
-        
+
         if (results.gestures.length > 0) {
           for (let i = 0; i < results.landmarks.length; i++) {
             if (categoryName != results.gestures[i][0].categoryName && results.gestures[i][0].categoryName != 'None') {
               categoryName = results.gestures[i][0].categoryName;
-              gestureChange(categoryName);
+              console.log(categoryName);
+              //gestureChange(categoryName);
             }
       
-            const iconName = (categoryName in gestureIconMap) ? gestureIconMap[categoryName] : 'icon_cursor';
+            //const iconName = (categoryName in gestureIconMap) ? gestureIconMap[categoryName] : 'icon_cursor';
            }
         } 
+        
         // Call this function again to keep predicting when the browser is ready.
-      
-        if (webcamRunning === true) {
+        if (true) {
           window.requestAnimationFrame(predictWebcam);
         }
-        */
+        
+        
       }
     };
     createGestureRecognizer();
@@ -97,20 +102,41 @@ function TestComponent() {
 
   return (
     <>
-      <div class="outer-container" style={{ position: "absolute", zIndex:10 }}>
-        <div id="webcamButtonContainer">
-          <div class="annotations">This prototype uses your webcam to recognize hand gestures.</div>
+      <div className="outerContainer" style={{ display: introOneDisplay, position: "absolute", zIndex:10 }}>
+        <div id="innerContainer">
+          Loading...
+        </div>
+      </div>
+      <div className="outerContainer" style={{ display: introTwoDisplay, position: "absolute", zIndex:10 }}>
+        <div id="innerContainer">
+          <div className="annotations">This prototype uses your webcam to recognize hand gestures.</div>
           <div ref={enableWebcamButtonRef}  id="webcamButton">
-            <span class="mdc-button__label">Enable Webcam</span>
+            <span className="mdc-button__label">Enable Webcam</span>
           </div> 
           <div style={{height:"100px"}}></div>
         </div>
       </div>
+      <div className="outerContainer" style={{ display: introThreeDisplay, position: "absolute", zIndex:10 }}>
+        <div id="innerContainer">
+         Staring camera...
+        </div>
+      </div>
+      <div className="outerContainer" style={{ display: introFourDisplay, position: "absolute", zIndex:10 }}>
+        <div id="innerContainer">
+          <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+            
+            <img src={process.env.PUBLIC_URL + '/svg/icon_palm_open.svg'} 
+              alt="open hand" 
+              style={{width:'60px', height:'60px'}}
+              />to open a dialog...
+          </div>
+        </div>
+      </div>
 
       <div style={{ position: "absolute", zIndex:10 }}>
-        <video ref={videoRef} s 
-          style={{ position: "absolute", display: "none", left: "0px", top: "0px", width: "100%", height: "100%" }} autoplay playsinline></video>
-        <canvas ref={canvasRef} class="output_canvas" id="output_canvas" 
+        <video ref={videoRef}
+          style={{ position: "absolute", display: "none", left: "0px", top: "0px", width: "100%", height: "100%" }} autoPlay playsInline></video>
+        <canvas ref={canvasRef} className="output_canvas" id="output_canvas" 
           style={{ position: "absolute", display: "none", left: "0px", top: "0px",  width: "100%", height: "100%" }}></canvas>
       </div>
     </>
